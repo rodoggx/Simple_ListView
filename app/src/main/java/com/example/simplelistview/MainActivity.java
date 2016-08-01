e7a59844f3951a744e6bc278c3a65418ae03009c
rodoggx <mikerodolph@yahoo.com>
2016-08-01 16:02:38 -0400

    commit 2

--- app/src/main/java/com/example/simplelistview/MainActivity.java
+++ app/src/main/java/com/example/simplelistview/MainActivity.java
@@ -5,11 +5,18 @@ import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
     private ListView mListView;
 
     private String[] mData =
             new String[] {"Chris", "Mike", "Aldo", "Karles", "Libu", "Lisa", "Gwen", "Mary", "Gina", "Tamekia", "Tom", "Bill", "Larry", "Keisha", "Beyonce", "Tina", "Jill", "Karen", "Joe", "Frank"};
+
+    private List<String> dataList =
+            Arrays.asList(mData);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +27,11 @@ public class MainActivity extends AppCompatActivity {
 
         //pass array of objects
     ArrayAdapter<String> arrayAdapter =
-            new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mData);
+            new ArrayAdapter<String>(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    dataList);
+
 
         //attach adapter to listview
         mListView.setAdapter(arrayAdapter);
